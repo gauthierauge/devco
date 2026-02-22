b57996bef6d259b31a57eabd6e9e25c7d2220c71
gauthierauge <gauthier.auge@2dcom.fr>
2026-02-22 09:37:44 +0100

    fix: inline variable

--- front/src/pages/Cart.ts
+++ front/src/pages/Cart.ts
@@ -1,7 +1,7 @@
-import { getProduct } from "../api/product.api";
-import { API_URL } from "../config/env";
-import { cartService } from "../services/cart.service";
-import { toView } from "../services/product.service";
+import {getProduct} from "../api/product.api";
+import {API_URL} from "../config/env";
+import {cartService} from "../services/cart.service";
+import {toView} from "../services/product.service";
 
 const renderEmptyCart = () => `
     <div class="page">
@@ -151,9 +151,7 @@ const Cart = () => {
         const { authService } = await import("@/services/auth.service");
         const isLoggedIn = authService.isLoggedIn();
 
-        // Afficher le panier
-        const html = await renderCart(isLoggedIn);
-        container.innerHTML = html;
+        container.innerHTML = await renderCart(isLoggedIn);
 
         mountCartActions();
     };
